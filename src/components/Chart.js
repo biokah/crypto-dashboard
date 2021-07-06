2744766b966063f4f6cfdc1ca19c7f730725f260
Maria Anais Reyes <maria.anaisr@gmail.com>
2021-07-05 22:37:03 -0500

    adding spec file

---
+++ src/components/Chart.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2';
+
+
+
+const Chart = () => {
+    return (
+        <div>
+            <Bar 
+                data={{
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [
+                        {
+                            label: '# of votes',
+                            data: [12, 19, 3, 5, 2, 3]
+                        }
+                    ]
+                }}
+                height={400}
+                width={600}
+                options={{
+                    maintainAspectRatio: false
+                }}
+            />
+        </div>
+        
+            
+    )
+}
+
+export default Chart;
